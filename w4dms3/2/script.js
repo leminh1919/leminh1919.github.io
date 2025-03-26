84b5a4c687ec6c9e3c4d78a863d8cba3460f5749
minhle1919 <s3997682@rmit.edu.vn>
2025-03-27 09:51:27 +1100

    w4dms3

---
+++ w4dms3/2/script.js
@@ -0,0 +1,45 @@
+/* find modal close button and add an eventlistener */
+document.getElementById("dialogCloseBtn").addEventListener("click", () => {
+  document.getElementById("introDialog").close();
+});
+
+/* to get the backdrop working we need to trigger modal open with js */
+document.getElementById("introDialog").showModal();
+
+function toneInit() {
+  mySynth.toDestination();
+  mySynth.triggerAttackRelease("C4", "8n");
+}
+
+let mySynth = new Tone.Synth();
+
+document.getElementById("synthTriggerBtn", () => {});
+
+/////////////// Representation of tone mental model ///////////////
+
+/* 
+Source          -------------->    Destination
+
+Synthesizer     .toDesination()    Audio Driver ( => Audio Hardware )
+Audio Player
+*/
+
+/* 
+Trigger             ---------------------->   Source          
+
+Pitch, Length       .triggerAttackRelease()   Synthesizer
+Pitch, Start Time   .triggerAttack()      
+End Time            .triggerRelease()
+
+Start Playback      .start()                  Audio Player
+Stop Playback       .stop()
+*/
+
+/* 
+Source          --------->    Effect        -------------->    Destination
+
+Synthesizer     .connect()    Reverb        .toDesination()    Audio Driver ( => Audio Hardware )
+Audio Player                  Phaser        OR
+                              Distortion    .connect()         Other Effect
+                              etc
+*/

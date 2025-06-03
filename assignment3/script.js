fa27d7711a689d9ba35fd259f6e0fb10bf832d75
minhle1919 <s3997682@rmit.edu.vn>
2025-06-04 01:44:33 +1000

    a3

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,11 +1,27 @@
 function showPopup(el) {
+  const popup = document.getElementById("popup");
+
+  // Set content
   document.getElementById("popup-image").src = el.dataset.image;
+  document.getElementById("popup-title").textContent = el.dataset.name;
   document.getElementById("popup-description").textContent =
     el.dataset.description;
   document.getElementById("popup-weapon-img").src = el.dataset.weaponimg;
-  document.getElementById("character-popup").classList.remove("hidden");
+
+  // Remove any previous side classes
+  popup.classList.remove("good-popup", "bad-popup");
+
+  // Add correct side class
+  if (el.dataset.side === "bad") {
+    popup.classList.add("bad-popup");
+  } else if (el.dataset.side === "good") {
+    popup.classList.add("good-popup");
+  }
+
+  // Show popup
+  popup.classList.remove("hidden");
 }
 
 function closePopup() {
-  document.getElementById("character-popup").classList.add("hidden");
+  document.getElementById("popup").classList.add("hidden");
 }

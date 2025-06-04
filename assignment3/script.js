f307ba30f77a87eef22e9d2cbaddc8be79f326f1
minhle1919 <s3997682@rmit.edu.vn>
2025-06-04 23:07:24 +1000

    a3 final

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,31 +1,30 @@
+//I want to create a window that pop up fullscreen but not jumping to another pages
+//When reload page always go to the top
 window.onload = function () {
   window.scrollTo(0, 0);
 };
+//Pop up when click on a character : read data for each character
 function showPopup(el) {
   const popup = document.getElementById("popup");
-
-  // Set content
   document.getElementById("popup-image").src = el.dataset.image;
   document.getElementById("popup-title").textContent = el.dataset.name;
   document.getElementById("popup-description").textContent =
     el.dataset.description;
   document.getElementById("popup-weapon-img").src = el.dataset.weaponimg;
 
-  // Remove any previous side classes
   popup.classList.remove("good-popup", "bad-popup");
 
-  // Add correct side class
+  //Seperate style for each side
   if (el.dataset.side === "bad") {
     popup.classList.add("bad-popup");
   } else if (el.dataset.side === "good") {
     popup.classList.add("good-popup");
   }
 
-  // Show popup
   popup.classList.remove("hidden");
   document.body.classList.add("noscroll");
 }
-
+//Close popup window
 function closePopup() {
   document.getElementById("popup").classList.add("hidden");
   document.body.classList.remove("noscroll");
